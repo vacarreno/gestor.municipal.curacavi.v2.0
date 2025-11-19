abc54e79dc23011e1530caee12bd28cad80fe06c
vacarreno <victor@conceptoweb.cl>
2025-11-18 22:40:33 -0300

    Actualización de archivos modificados

--- server/app.js
+++ server/app.js
@@ -66,7 +66,7 @@ app.use(
 );
 
 // === PREFLIGHT EXPRESS 5 ===
-app.options("*", (req, res) => {
+app.options("/*", (req, res) => {
   res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
